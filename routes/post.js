b332b3b65f1cb781b91bba8873a46d1b5214af47
Cobbleopolis <agounimon@yahoo.com>
2015-08-02 02:43:34 -0400

    Moved some stuff around

--- routes/post.js
+++ routes/post.js
@@ -4,7 +4,7 @@ var app = express();
 
 
 app.use('/index/getSlideshow', function (req, res) {
-    fs.readdir(__dirname + '/../public/images/slideshow', function (err, files) {
+    fs.readdir(__dirname + '/../public/images/home/slideshow', function (err, files) {
         if (err)
             throw err;
         res.send(files);
