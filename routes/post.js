d64cf8ed42e739b91238a414f1eba4c6e7927fb5
Cobbleopolis <agounimon@yahoo.com>
2015-08-09 18:38:53 -0400

    Added the /post/cakes/getAlbums endpoint

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 var app = express();
 
 
@@ -14,5 +15,21 @@ app.use('/index/getSlideshow', function (req, res) {
         res.send(files);
     })
 });
+
+app.use('/cakes/getAlbums', function (req, res) {
+    var srcpath = __dirname + "/../public/images/gallery";
+    var albumLocs = fs.readdirSync(srcpath).filter(function(file) {return(fs.statSync(path.join(srcpath, file)).isDirectory());});
+    var albums = {};
+    for (var i in albumLocs) {
+        var entry = [];
+        entry[0] = albumLocs[i];
+        entry[1] = fs.readdirSync(srcpath + "/" + albumLocs[i])[0];
+        albums[i] = entry;
+    }
+    console.log(albums);
+    res.send(albums);
+});
+
+
 
 module.exports = app;
