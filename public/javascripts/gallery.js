506cf86408b02e3672cc879ae8dd6398f2a2d842
Cobbleopolis <agounimon@yahoo.com>
2015-09-29 17:32:43 -0400

    Images now loaded into table

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -18,20 +18,50 @@ $(window).load(function() {
     //});
     $(".secondNavListItem").on("click", function() {  //use a class, since your ID gets mangled
         getAlbumsImages($(this)[0].innerHTML, function(){
-            var loadingDiv = $("#viewDiv");
+            var loadingDiv = $("#loadingDiv");
+            var viewDiv = $("#galleryTable");
             loadingDiv.empty();
-            var html = "";
-            albumImages.forEach(function (element) { html += '<img class="loadingImage" src="//static.sweeteventsjc.com/images/gallery/' + currentAlbum + '/' + element + '">'});
-            loadingDiv.append(html);
+            viewDiv.empty();
+            var html = "<tr>";
+            var i = 0;
+            albumImages.forEach(function (element) {
+                if (i % 4 === 0 && i !== 0) html += "</tr><tr>";
+                html += '<td><img class="galleryImage" src="//static.sweeteventsjc.com/images/gallery/' + currentAlbum + '/' + element + '"></td>';
+                i++;
+            });
+            viewDiv.append(html);
+            //$(".loadingImage").on("load", function() {
+            //    var image = $($(this)[0]);
+            //    //console.log("Image loaded. | " + image.outerHTML + " | " + image.attr("src"));
+            //    var img = $("<img>");
+            //    img.attr("src", image.attr("src"));
+            //    viewDiv.append(img);
+            //    thing = image;
+            //    //console.log(thing);
+            //});
         });
 
     });
-    $(".loadingImage").load(function() {  //use a class, since your ID gets mangled
-        console.log($(this));
-        //$("#viewDiv").append($(this))
-    });
-
+    //$(function(a){
+    //    var addToAll = false;
+    //    var gallery = true;
+    //    var titlePosition = 'inside';
+    //    $(addToAll ? 'img' : 'img.fancybox').each(function(){
+    //        var $this = $(this);
+    //        var title = $this.attr('title');
+    //        var src = $this.attr('data-big') || $this.attr('src');
+    //        var a = $('<a href="#" class="fancybox"></a>').attr('href', src).attr('title', title);
+    //        $this.wrap(a);
+    //    });
+    //    if (gallery)
+    //        $('a.fancybox').attr('rel', 'fancyboxgallery');
+    //    $('a.fancybox').fancybox({
+    //        titlePosition: titlePosition
+    //    });
+    //});
+    //$.noConflict();
 });
+
 
 function getAlbumsImages(album, callback) {
     $.ajax({
